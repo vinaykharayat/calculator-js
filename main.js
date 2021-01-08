943f4cd45381765cc61d80e22a443441a629bd29
vinaykharayat <vinaykharayat@gmail.com>
2021-01-08 22:48:33 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+var input = null;
+let inputArray = [];
+let oprPressed = false;
+let first;
+let second;
+let opr;
+Array.from(document.querySelectorAll("#calculator_buttons div")).forEach(element => {
+    element.addEventListener("click", function () {
+        switch (element.innerHTML) {
+            case "=":
+                calc(opr, first, second);
+            case "+":
+                first =document.querySelector("h1").innerHTML;
+                oprPressed = true;
+                opr = element.innerHTML;
+                console.log(opr);
+                break;
+            case "-":
+                first =document.querySelector("h1").innerHTML;
+                oprPressed = true;
+                opr = element.innerHTML;
+                break;
+            case "/":
+                first =document.querySelector("h1").innerHTML;
+                oprPressed = true;
+                opr = element.innerHTML;
+                break;
+            case "*":
+                first =document.querySelector("h1").innerHTML;
+                oprPressed = true;
+                opr = element.innerHTML;
+                break;
+            default:
+                if(!oprPressed){
+                    document.querySelector("h1").textContent+=element.innerHTML;
+                }else{
+                    document.querySelector("h1").textContent+= element.innerHTML;
+                    second += element.innerHTML;
+                }
+                break;
+        }
+
+    }, false);
+});
+
+function calc(opr, first, second) {
+    console.log(second.slice(9, second.length));
+    console.log(parseInt(first) + parseInt(second.slice(9, second.length)));
+    switch (opr) {
+        case "+":
+            document.querySelector("h1").textContent = parseInt(first) + parseInt(second.slice(9, second.length));
+            break;
+        case "-":
+            document.querySelector("h1").textContent = parseInt(first) - parseInt(second.slice(9, second.length));
+            break;
+        case "/":
+            document.querySelector("h1").textContent = parseInt(first) / parseInt(second.slice(9, second.length));
+            break;
+        case "*":
+            document.querySelector("h1").textContent = parseInt(first) * parseInt(second.slice(9, second.length));
+            break;
+    }
+}
+
+function showResult() {
+
+}
+
+function clear() {
+    inputArray = [];
+    document.querySelector("h1").textContent = "";
+}
