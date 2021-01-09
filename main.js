b72f9d247002d9bef282b0077be9568ef6c32657
vinaykharayat <vinaykharayat@gmail.com>
2021-01-09 10:06:02 +0530

    fixed "+" coming in result

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 var input = null;
 let inputArray = [];
 let oprPressed = false;
-let first;
-let second;
-let opr;
+let first = null;
+let second = null;
+let opr = null;
 Array.from(document.querySelectorAll("#calculator_buttons div")).forEach(element => {
     element.addEventListener("click", function () {
         switch (element.innerHTML) {
             case "=":
                 calc(opr, first, second);
+                break;
             case "+":
-                first =document.querySelector("h1").innerHTML;
-                document.querySelector("h1").textContent+= "+";
+                first = document.querySelector("h1").innerHTML;
+                document.querySelector("h1").textContent += "+";
                 oprPressed = true;
                 opr = element.innerHTML;
-                console.log(opr);
                 break;
             case "-":
-                first =document.querySelector("h1").innerHTML;
-                document.querySelector("h1").textContent+= "-";
+                first = document.querySelector("h1").innerHTML;
+                document.querySelector("h1").textContent += "-";
                 oprPressed = true;
                 opr = element.innerHTML;
                 break;
             case "/":
-                first =document.querySelector("h1").innerHTML;
-                document.querySelector("h1").textContent+= "/";
+                first = document.querySelector("h1").innerHTML;
+                document.querySelector("h1").textContent += "/";
                 oprPressed = true;
                 opr = element.innerHTML;
                 break;
             case "*":
-                first =document.querySelector("h1").innerHTML;
-                document.querySelector("h1").textContent+= "*";
+                first = document.querySelector("h1").innerHTML;
+                document.querySelector("h1").textContent += "*";
                 oprPressed = true;
                 opr = element.innerHTML;
                 break;
             default:
-                if(!oprPressed){
-                    document.querySelector("h1").textContent+=element.innerHTML;
-                }else{
-                    document.querySelector("h1").textContent+= element.innerHTML;
-                    second += element.innerHTML;
+                if (!oprPressed) {
+                    document.querySelector("h1").textContent += element.innerHTML;
+                } else {
+                    document.querySelector("h1").textContent += element.innerHTML;
+                    if (second !== null) {
+                        second += element.innerHTML;
+                    } else {
+                        second = element.innerHTML;
+                    }
                 }
                 break;
         }
@@ -48,30 +52,28 @@ Array.from(document.querySelectorAll("#calculator_buttons div")).forEach(element
 });
 
 function calc(opr, first, second) {
-    console.log(second.slice(9, second.length));
-    console.log(parseInt(first) + parseInt(second.slice(9, second.length)));
     switch (opr) {
         case "+":
-            document.querySelector("h1").textContent = parseInt(first) + parseInt(second.slice(9, second.length));
-            document.querySelector("h1").textContent.slice(document.querySelector("h1").textContent.length-1, document.querySelector("h1").textContent.length);
+            document.querySelector("h1").textContent = parseInt(first) + parseInt(second);
             break;
         case "-":
-            document.querySelector("h1").textContent = parseInt(first) - parseInt(second.slice(9, second.length));
+            document.querySelector("h1").textContent = parseInt(first) - parseInt(second);
             break;
         case "/":
-            document.querySelector("h1").textContent = parseInt(first) / parseInt(second.slice(9, second.length));
+            document.querySelector("h1").textContent = parseInt(first) / parseInt(second);
             break;
         case "*":
-            document.querySelector("h1").textContent = parseInt(first) * parseInt(second.slice(9, second.length));
+            document.querySelector("h1").textContent = parseInt(first) * parseInt(second);
+            break;
+
+        default:
+            console.log(first);
+            console.log(document.querySelector("h1").textContent);
             break;
     }
 }
 
-function showResult() {
-
-}
-
-document.querySelector("button").addEventListener("click",function() {
+document.querySelector("button").addEventListener("click", function () {
     inputArray = [];
     document.querySelector("h1").textContent = "";
 });
